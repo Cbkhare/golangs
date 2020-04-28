7291edae62f212847ccbf8687e615cc9f0008c6d
Chitrabasu Khare <Cbkhare@users.noreply.github.com>
2020-04-28 14:02:11 +0530

    CF

---
+++ CF_1343_B.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var a, b int
+	fmt.Scan(&a)
+	for i := 0; i < a; i++ {
+		fmt.Scan(&b)
+		if b%2 != 0 || (b/2)%2 != 0 {
+			fmt.Println("No")
+		} else {
+			fmt.Println("Yes")
+			var c []string
+			sum := 0
+			sum1 := 0
+			for j := 0; j < b; j += 2 {
+				c = append(c, strconv.Itoa(j+2))
+				sum += j + 2
+			}
+			for j := 1; j < b-2; j += 2 {
+				// b-2 to append last value in line 28
+				c = append(c, strconv.Itoa(j))
+				sum1 += j
+			}
+			c = append(c, strconv.Itoa(sum-sum1))
+			fmt.Println(strings.Join(c, " "))
+		}
+	}
+}
+
+
+/*
+https://codeforces.com/problemset/problem/1343/B
+Hint:
+sum(half even) == even
+sum(half odd) == even, only when the len(odd number)==even
+*/
